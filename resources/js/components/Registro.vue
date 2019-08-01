026e0d61dc22aa57fb4f90bf5c763b9969bbd36d
Rafael Framil <raframil@gmail.com>
2019-07-31 22:28:14 -0300

    registro

--- resources/js/components/Registro.vue
+++ resources/js/components/Registro.vue
@@ -163,7 +163,7 @@ export default {
   methods: {
     // G Recaptcha
     onVerify: function(response) {
-      console.log(response);
+      //console.log(response);
       this.captchaResponse = response;
     },
     onExpired: function() {
